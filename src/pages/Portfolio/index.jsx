6d745fb38bf4eb145e6c3be004721c22127a5484
Victor Mielke <vicemanu31@outlook.com>
2023-07-26 19:51:29 -0300

    ajeitando o codigo do portfolio

--- src/pages/Portfolio/index.jsx
+++ src/pages/Portfolio/index.jsx
@@ -1,9 +1,14 @@
-
+import './portfolio.css'
 
 export default function Portfolio() {
     return(
-        <>
-        
-        </>
+        <div>
+            <header>
+                
+                </header>
+                <main>
+                    
+                </main>
+        </div>
     )
 }
