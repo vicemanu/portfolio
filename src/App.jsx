3d562b8216b256a27de54b242103a7331383bea1
Victor Mielke <vicemanu31@outlook.com>
2023-07-26 19:36:48 -0300

    começando o projeto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Router from "./Router"
+
+
+function App() {
+ 
+  return (
+    <>
+      <Router/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Router.jsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+import Portfolio from "./pages/portfolio";
+import Admin from "./pages/Admin";
+
+
+export default function Router() {
+    return(
+        <Routes>
+            <Route path="/" element={<Portfolio/>}/>
+            <Route path="/admin" element={<Admin/>}/>
+        </Routes>
+    )
+}
