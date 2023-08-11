62eee2938c6b8450c734de631e701cf6c2e3bc88
Victor Mielke <vicemanu31@outlook.com>
2023-08-11 00:20:43 -0300

    criando elemento de visualização do projeto

--- src/components/Projects/Boxproject.jsx
+++ src/components/Projects/Boxproject.jsx
@@ -5,12 +5,12 @@ import Githubsvg from './github.svg'
 export default function Boxproject(props) {
   
     return (
-        <a href={props.link} target='_blank' className='carrossel-projects_box-projects'>
+        <div onClick={()=> {props.setProjetoTela(true); props.setNumProjetoTela(props.index)}} href={props.link} target='_blank' className='carrossel-projects_box-projects'>
         <img className='box-projects__img' src={props.img} alt="imagem projeto" />
         <h3>{props.nome1}<br/>{props.nome2}</h3>
         <p>{props.desc}</p>
-        <a className="github" target='_blank' href={props.linkgithub}><img src={Githubsvg} alt="" /></a>
-    </a>
+        <a className="github" target='_blank' href={props.linkGithub}><img src={Githubsvg} alt="" /></a>
+    </div>
       
     )
   }
--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,17 +1,18 @@
+/* eslint-disable react/jsx-no-target-blank */
 import './projects.css'
 import Boxproject from './Boxproject'
 import { collection, getDocs } from 'firebase/firestore';
 import { db } from '../../firebase';
 import { useEffect, useState } from 'react';
-
-
-
+import Githubsvg from './github.svg'
 
 
 
 export default function Projects() {
   
   const [data, setData] = useState([])
+  const [projetoTela, setProjetoTela] = useState(false)
+  const [numProjetoTela, setNumProjetoTela] = useState()
 
   useEffect(()=> {
     async function buscarprojetos() {
@@ -24,10 +25,12 @@ export default function Projects() {
           lista.push({
             title1: doc.data().title1,
             title2: doc.data().title2,
+            subtitle: doc.data().subtitle,
             linkSite: doc.data().linkSite,
             imgProject: doc.data().imgProject,
-            subtitle: doc.data().subtitle,
             linkGithub: doc.data().linkGithub,
+            descricao: doc.data().descricao,
+            habilidades: doc.data().habilidades,
             id: doc.id
   
           })
@@ -47,10 +50,26 @@ export default function Projects() {
         <div className='projects__carrossel-projects'>
 
 
-          {data?.map(e => {
-          return <Boxproject key={e.id} link={e.linkSite} img={e.imgProject} nome1={e.title1} nome2={e.title2} desc={e.subtitle} linkgithub={e.linkGithub}/>
+          {data?.map((e, index) => {
+          return <Boxproject key={e.id} link={e.linkSite} img={e.imgProject} nome1={e.title1} nome2={e.title2} desc={e.subtitle} linkgithub={e.linkGithub} setProjetoTela={setProjetoTela} setNumProjetoTela={setNumProjetoTela} index={index}/>
         })} 
         </div>
+        {projetoTela && <div className='projetotela'>
+            <div className='projetotela--box'>
+            <button className='close' onClick={() => setProjetoTela(false)}><i className="bi bi-x"></i></button>
+            <h3 className='projetotela__box--title'>{data[numProjetoTela]?.title1} {data[numProjetoTela]?.title2}</h3>
+            <ul className='projetotela__box--habilidade__lista'>
+              {data[numProjetoTela].habilidades.map((e, index) => {
+                return(
+                  <li key={index}>{e}</li>
+                )
+              })}
+             </ul>
+            <p className='projetotela__box--text'>{data[numProjetoTela]?.descricao}</p>
+              <a className='projetotela__box--link' target="_blank" href={data[numProjetoTela]?.linkSite}>Ver Projeto <i className="bi bi-box-arrow-up-right"></i></a>
+              <a className="github--projeto" target='_blank' href={data[numProjetoTela]?.linkGithub}><img src={Githubsvg} alt="" /></a>
+            </div>
+          </div>}
       </section>
     )
   }
